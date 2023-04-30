d9df26f6d0772c0d6521dbd96673f7764c3d7019
jamolmahmudov <jamolmahmudov27102003@gmail.com>
2023-04-30 11:38:13 +0500

    6

--- List06.py
+++ List06.py
@@ -6,4 +6,6 @@ def main(fruits):
     Returns:
         int: return answer
     """
-    return
+    X=fruits.count('apple')
+    return X
+print(main(["apple", "banana", "apple", "apple", "apple"]))
